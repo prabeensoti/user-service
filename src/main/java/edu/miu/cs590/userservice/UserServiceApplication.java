f175c7b9e5ded1904627f71fd3f5576ae5a621e2
sourav40 <souravshrestha40@gmail.com>
2022-09-29 06:16:07 -0500

    consul removed.

--- src/main/java/edu/miu/cs590/userservice/UserServiceApplication.java
+++ src/main/java/edu/miu/cs590/userservice/UserServiceApplication.java
@@ -3,10 +3,9 @@ package edu.miu.cs590.userservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class UserServiceApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
